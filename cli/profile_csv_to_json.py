c9985a80b3c7e99a7b9523f2f1be32bddfa61a07
HammadM2003 <113640036+HammadM2003@users.noreply.github.com>
2022-09-15 22:50:29 -0700

    Delete cli directory

--- cli/profile_csv_to_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import json
-from rush import read_profiles_from_file
-import sys
-print(json.dumps(read_profiles_from_file(sys.argv[1])))
-
-# read_profiles_from_file
--- cli/rcfarm/mitm/go.mod
+++
@@ -1,8 +0,0 @@
-module mitm
-
-go 1.14
-
-require (
-	github.com/andybalholm/brotli v1.0.1
-	github.com/elazarl/goproxy v0.0.0-20201021153353-00ad82a08272
-)
